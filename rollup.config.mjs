3d767f5ce23f1fde253111e4b9d2388d328a4b48
BlueZeeKing <github@blueish.dev>
2023-01-17 20:59:37 -0500

    Initial commit

---
+++ rollup.config.mjs
@@ -0,0 +1,7 @@
+export default {
+	input: 'out/index.js',
+	output: {
+		file: 'index.cjs',
+		format: 'cjs'
+	}
+};
---
+++ src-electron/index.ts
@@ -0,0 +1,25 @@
+import { app, BrowserWindow } from 'electron';
+
+const createWindow = () => {
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600
+	});
+
+	win.loadURL('http://localhost:5173/');
+};
+
+async function main() {
+	await app.whenReady();
+	createWindow();
+
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) createWindow();
+	});
+
+	app.on('window-all-closed', () => {
+		if (process.platform !== 'darwin') app.quit();
+	});
+}
+
+main();
